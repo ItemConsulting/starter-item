9313b08d25b0cb1de01b0142634eb7206e63bb58
Christian Westgaard <comlock@gmail.com>
2024-05-13 09:57:22 +0200

    client-asset -> client

--- tsup.config.ts
+++ tsup.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(async (options: Options) => {
     return import('./tsup/server').then(m => m.default());
   }
   if (options.d === DIR_DST_ASSETS) {
-    return import('./tsup/client-asset').then(m => m.default());
+    return import('./tsup/client').then(m => m.default());
   }
   throw new Error(`Unconfigured directory:${options.d}!`)
 });
