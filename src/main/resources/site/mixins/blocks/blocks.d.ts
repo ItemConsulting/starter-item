90b17ab0526ae89f02eede926d520b2ad3652a37
Tom Arild Jakobsen <tajakobsen@gmail.com>
2024-08-06 08:06:34 +0200

    Add npm-library for blocks including css

--- src/main/resources/site/mixins/blocks/blocks.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Component } from "@enonic-types/core";
-import type { Blocks as BlocksRaw } from ".";
-
-export type BlockProcessorParams = {
-  component: Component;
-  locale: string;
-};
-
-export declare function process(config: BlocksRaw, params?: BlockProcessorParams): string;
--- src/main/resources/site/parts/blocks-view/blocks-view.stories.ts
+++ src/main/resources/site/parts/blocks-view/blocks-view.stories.ts
@@ -1,6 +1,12 @@
 import id from "./blocks-view.ftl";
+import "./blocks-view.css";
+import DetailsAnimated from "@itemconsulting/details-animated";
 import type { Meta, StoryObj } from "@itemconsulting/xp-storybook-utils";
 import type { FreemarkerParams } from "./blocks-view.freemarker";
+
+if (!window.customElements.get("details-animated")) {
+  window.customElements.define("details-animated", DetailsAnimated);
+}
 
 const meta: Meta<FreemarkerParams> = {
   title: "Part/Blocks View",
@@ -15,8 +21,15 @@ const meta: Meta<FreemarkerParams> = {
             [#include "/site/mixins/blocks-text/blocks-text.ftl" /]
           [/#assign]
 
-          [#assign blocks]
-            [#assign blocks=[text1] /]
+          [#assign accordion]
+            [#-- Accordion --]
+            [#assign items=accordionItems /]
+            [#assign classes=accordionClasses /]
+            [#include "/site/mixins/blocks-accordion/blocks-accordion.ftl"]
+          [/#assign]
+
+          [#assign processedBlocks]
+            [#assign blocks=[text1, accordion] /]
             [#include "/site/mixins/blocks/blocks.ftl" /]
           [/#assign]
 
@@ -46,5 +59,26 @@ export const blocksView: StoryObj = {
         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
     `,
+    accordionItems: [
+      {
+        title: "Overskrift 1",
+        text: `
+          <p>Dette er noe riktekst</p>
+          <ul>
+            <li>test</li>
+            <li>test2</li>
+          </ul>`,
+      },
+      {
+        title: "Overskrift 2",
+        text: `
+          <p>Dette er noe riktekst</p>
+          <ul>
+           <li>test</li>
+           <li>test2</li>
+          </ul>`,
+      },
+    ],
+    accordionClasses: "theme-neutral",
   },
 };
